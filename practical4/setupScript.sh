677c9159f66048cd5626649503d6895472e7764a
murchant <mamurtag@tcd.ie>
2018-10-15 16:50:56 +0100

    setup gcloud

---
+++ practical4/setupScript.sh
@@ -0,0 +1,7 @@
+sudo apt update
+sudo apt upgrade -y
+sudo apt-get install build-essential -y
+sudo apt-get install linux-image-extra-virtual -y
+sudo /bin/bash NVIDIA-Linux-x86_64-367.128.run
+sudo apt install hashcat -y
+hashcat -I
